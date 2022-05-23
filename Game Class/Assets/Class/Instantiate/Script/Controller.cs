b073360487eba24d02a4b2b14a110ca81a23824a
mol1986@naver.com <kileab@naver.com>
2022-05-23 21:44:40 +0900

    Trigger System

--- Game Class/Assets/Class/Instantiate/Script/Controller.cs
+++ Game Class/Assets/Class/Instantiate/Script/Controller.cs
@@ -25,38 +25,6 @@ public class Controller : MonoBehaviour
         // 벡터의 크기를 1로 만들어 줍니다.
 
         transform.Translate(dir.normalized * speed * Time.deltaTime);
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.position = new Vector3
-                (
-                   transform.position.x,
-                   3,
-                   transform.position.z
-                );
-        }
     }
-
-    // 물리적인 충돌을 했을 때 동작하는 함수입니다.
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collision");
-    }
-
-    // 물리적인 충돌을 하고 있을 때 동작하는 함수입니다.
-    private void OnCollisionStay(Collision collision)
-    {
-        Debug.Log("Stay");
-    }
-
-    // 물리적인 충돌을 벗어났을 때 동작하는 함수입니다.
-    private void OnCollisionExit(Collision collision)
-    {
-        Debug.Log("Exit");
-    }
-
-    // 충돌 조건 
-    // 2개의 오브젝트 중 하나는 RigidBody를 가지고 있어야 합니다.
-    // 2개의 모두 Collider 컴포넌트를 가지고 있어야 합니다.
 
 }
